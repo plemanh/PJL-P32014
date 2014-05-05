c89869ecfbb228e3a42cac87e9cdf49296e42b5f
plemanh <lemanh@enst.fr>
2014-05-05 21:48:55 +0200

    Update script_createTokenizedData.py

--- Documents/Telecom/PJL2014P3/script_createTokenizedData.py
+++ Documents/Telecom/PJL2014P3/script_createTokenizedData.py
@@ -24,7 +24,7 @@ listNegEmoticons = ["^o)",":_(",":-(",":-(o)",":-/",":-&",":-|",":-S",":,(",":â€
 
 dicoSlang = {"121":"one to one", "a/s/l":"age sex location", "adn":"any day now", "afaik":"as far as I know", "afk":"away from keyboard", "aight":"alright", "alol":"actually laughing out loud","b4"	: "before","b4n"	: "bye for now","bak"	: "back at the keyboard","bf"	: "boyfriend","bff"	: "best friends forever","bfn"	: "bye for now","bg"	: "big grin","bta"	: "but then again","btw"	: "by the way","cid"	: "crying in disgrace","cnp"	: "continued in my next post","cp"	: "chat post","cu"	: "see you","cul" 	: "see you later","cul8r"	: "see you later","cya"	: "bye","cyo"	: "see you online","dbau"	: "doing business as usual","fud"	: "fear, uncertainty, and doubt","fwiw"	: "for what it's worth","fyi"	: "for your information","g"	: "grinâ€ ","g2g"	: "got to go","ga"	: "go ahead","gal"	: "get a life","gf"	: "girlfriend","gfn"	: "gone for now","gmbo"	: "giggling my butt off","gmta"	: "great minds think alike","h8"	: "hate","hagn"	: "have a good night","hdop"	: "help delete online predators","hhis"	: "hanging head in shame","iac"	: "in any case","ianal"	: "I am not a lawyer","ic"	: "I see","idk"	: "I don't know","imao"  : "in my arrogant opinion","imnsho": "in my not so humble opinion","imo"	: "in my opinion","iow"	: "in other words","ipn"	: "I'm posting naked","irl"	: "in real life","jk"	: "just kidding","l8r"	: "later","ld"	: "later, dude","ldr"	: "long distance relationship","llta"	: "lots and lots of thunderous applause","lmao"	: "laugh my ass off","lmirl"	: "let's meet in real life","lol"	: "laugh out loud","ltr"	: "longterm relationship","lulab"	: "love you like a brother","lulas"	: "love you like a sister","luv"	: "love","m/f"	: "male or female","m8"	: "mate","milf"	: "mother I would like to fuck","oll"	: "online love","omg"	: "oh my god","otoh"	: "on the other hand","pir"	: "parent in room","ppl"	: "people","r"	: "are","rofl"	: "roll on the floor laughing","rpg"	: "role playing games","ru"	: "are you","shid"	: "slaps head in disgust","somy"	: "sick of me yet","sot"	: "short of time","thanx"	: "thanks","thx"	: "thanks","ttyl"	: "talk to you later","u"	: "you","ur" 	: "you are","uw"	: "you are welcome","wb"	: "welcome back","wfm"	: "works for me","wibni"	: "wouldn't it be nice if","wtf"	: "what the fuck","wtg"	: "way to go","wtgp"	: "want to go private","ym"	: "young man"}
 
-
+#Creation of the data set
 usedData = []
 i = 0
 while i < 450:
@@ -32,6 +32,9 @@ while i < 450:
     usedData.append(tweet)
     data.remove(tweet)
     i = i+1     
+
+
+#Test for version 1
 
 nbPos = 0
 nbNeg = 0
@@ -103,7 +106,8 @@ print('2-%d -%d -%d -' %(nbNeutralInPos, nbNeutralInNeutral, nbNeutralInNeg))
 print('0-%d -%d -%d -' %(nbNegInPos, nbNegInNeutral, nbNegInNeg))
 
 
-#Script2 : Sentiment extracting for 300 elements 
+#Test for version 2 
+
 nbPos = 0
 nbNeg = 0
 nbNeutral = 0
@@ -174,7 +178,7 @@ print('2-%d -%d -%d -' %(nbNeutralInPos, nbNeutralInNeutral, nbNeutralInNeg))
 print('0-%d -%d -%d -' %(nbNegInPos, nbNegInNeutral, nbNegInNeg))
 
 
-#Script3 : Sentiment extracting for 450 tweets with verson 3 
+#Test for version 3
 nbPos = 0
 nbNeg = 0
 nbNeutral = 0
@@ -245,7 +249,7 @@ print('2-%d -%d -%d -' %(nbNeutralInPos, nbNeutralInNeutral, nbNeutralInNeg))
 print('0-%d -%d -%d -' %(nbNegInPos, nbNegInNeutral, nbNegInNeg))
 
 
-#Script4 : Sentiment extracting for 450 tweets with verson 3 
+#Test for version 4
 nbPos = 0
 nbNeg = 0
 nbNeutral = 0
@@ -316,7 +320,8 @@ print('2-%d -%d -%d -' %(nbNeutralInPos, nbNeutralInNeutral, nbNeutralInNeg))
 print('0-%d -%d -%d -' %(nbNegInPos, nbNegInNeutral, nbNegInNeg))
 
 
-#Script5 : Sentiment extracting for 450 tweets with verson 3 
+#Test for version 5
+
 nbPos = 0
 nbNeg = 0
 nbNeutral = 0
@@ -387,7 +392,8 @@ print('2-%d -%d -%d -' %(nbNeutralInPos, nbNeutralInNeutral, nbNeutralInNeg))
 print('0-%d -%d -%d -' %(nbNegInPos, nbNegInNeutral, nbNegInNeg))
 
 
-#Script6 : Sentiment extracting for 450 tweets with verson 6 
+#Test for version 6
+
 nbPos = 0
 nbNeg = 0
 nbNeutral = 0
